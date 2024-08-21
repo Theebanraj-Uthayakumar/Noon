4722b3f8484cb63ee6dfb9aad3eda1ac1e5e254f
Theeban <theebanrajuthayakumar10@gmail.com>
2024-08-21 23:10:02 +0530

    FooterNav improvement

--- src/components/FooterNav/index.tsx
+++ src/components/FooterNav/index.tsx
@@ -8,13 +8,23 @@ const FooterNav = () => {
   return (
     <FooterContainer>
       <NavIcon>
-        <Link href="/" passHref>
+        <Link
+          href="/"
+          passHref
+          style={{ display: "flex", flexDirection: "row", gap: "10px" }}
+        >
           <FaHome />
+          <NavLabel>Home</NavLabel>
         </Link>
       </NavIcon>
       <NavIcon>
-        <Link href="/favourites" passHref>
+        <Link
+          href="/favourites"
+          passHref
+          style={{ display: "flex", flexDirection: "row", gap: "10px" }}
+        >
           <FaHeart />
+          <NavLabel>Liked</NavLabel>
         </Link>
       </NavIcon>
     </FooterContainer>
@@ -33,10 +43,27 @@ const FooterContainer = styled.div`
   bottom: 0;
   width: 100%;
   max-width: 585px;
+
+  @media (min-width: 600px) {
+    top: 0;
+    bottom: auto;
+    // padding: 20px;
+  }
 `;
 
 const NavIcon = styled.div`
   font-size: 24px;
   color: #f8f8f8;
   cursor: pointer;
+`;
+
+const NavLabel = styled.span`
+  display: none;
+  font-size: 12px;
+  color: #f8f8f8;
+  margin-top: 4px;
+
+  @media (min-width: 600px) {
+    display: block;
+  }
 `;
--- src/components/PostList/index.tsx
+++ src/components/PostList/index.tsx
@@ -1,7 +1,11 @@
 import styled from "styled-components";
 
 const PostList = styled.div`
-    margin-bottom: 50px;
+  margin-bottom: 50px;
+
+  @media (min-width: 600px) {
+    margin-top: 50px;
+  }
 `;
 
 export default PostList;
